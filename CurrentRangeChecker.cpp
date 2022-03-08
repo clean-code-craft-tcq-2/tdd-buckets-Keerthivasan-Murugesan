22d8f6d6c214e05f5c44587be3670a3e1dcd76f4
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-03-09 00:04:33 +0530

    Update CurrentRangeChecker.cpp

--- CurrentRangeChecker.cpp
+++ CurrentRangeChecker.cpp
@@ -14,6 +14,6 @@ TEST_CASE("Checks if range and count is updated with multiple range of sorted cu
 }
 
 TEST_CASE("Checks if range and count is updated with standalone and multiple range sorted current values") {
-    int CurrentValues[] = {2,4,5,7,8,10};
-	REQUIRE(CurrentRangeUpdate(CurrentValues, 6) == 4);
+    int CurrentValues[] = {2,4,5,7,8};
+	REQUIRE(CurrentRangeUpdate(CurrentValues, 5) == 3);
 }
