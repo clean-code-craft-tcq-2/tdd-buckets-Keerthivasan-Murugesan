0cf25301611e6f59b75af0b3264ebe3efd29cde6
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-03-08 23:52:05 +0530

    Update CurrentRangeChecker.cpp

--- CurrentRangeChecker.cpp
+++ CurrentRangeChecker.cpp
@@ -9,6 +9,6 @@ TEST_CASE("Checks if count is updated with single continuous range of current va
 }
 
 TEST_CASE("Checks if range and count is updated with multiple range of sorted current values") {
-    int CurrentValues[] = {3,3,4,5,7,8,10,11};
-	CHECK(CurrentRangeUpdate(CurrentValues, 8) == 3);
+    int CurrentValues[] = {4,5};
+	REQUIRE(CurrentRangeUpdate(CurrentValues, 2) == 1);
 }
